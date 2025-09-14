1fe9a167a8a7f39b1b363ccb70f4623c33b21773
Victor Hugo Prado Gomes <vitorvex@gmail.com>
2025-09-14 20:34:28 -0300

    feat: finalizado projeto com ajustes em variaveis de ambiente e publicação

---
+++ ItemService/EventProcessor/ProcessaEvento.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using ItemService.Data;
+using ItemService.Dtos;
+using ItemService.Models;
+
+namespace ItemService.EventProcessor
+{
+    public class ProcessaEvento : IProcessaEvento
+    {
+        private readonly IMapper _mapper;
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public ProcessaEvento(IMapper mapper, IServiceScopeFactory scopeFactory)
+        {
+            _mapper = mapper;
+            _scopeFactory = scopeFactory;
+        }
+
+        public void Processa(string mensagem)
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var itemRepository = scope.ServiceProvider.GetRequiredService<IItemRepository>();
+
+            var restauranteDto = _mapper.Map<RestauranteReadDto>(mensagem);
+
+            var restaurante = _mapper.Map<Restaurante>(restauranteDto);
+
+            if(!itemRepository.ExisteRestauranteExterno(restaurante.Id))
+            {
+                itemRepository.CreateRestaurante(restaurante);
+                itemRepository.SaveChanges();
+            }
+        }
+    }
+}
